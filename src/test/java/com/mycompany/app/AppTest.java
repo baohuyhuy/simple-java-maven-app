9aa3b1f10cc4e9f30e8017909835032ea07773fd
Bao Huy Pham <84869929+baohuyhuy@users.noreply.github.com>
2026-03-28 17:51:21 +0700

    Update test to check new app message

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("Hello World!", app.getMessage());
+        assertEquals("Hello World! My name is Huy", app.getMessage());
     }
 }
